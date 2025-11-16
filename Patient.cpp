bfc5a4354da385322207e9a37c0ff569f62c3151
julien <ju_clav@live.concordia.ca>
2025-11-16 17:24:39 -0500

    Patient body done

--- Patient.cpp
+++ Patient.cpp
@@ -1,8 +1,87 @@
 #include "Patient.h"
 #include <iostream>
 
-bool IsDischarged(Patient a) {
-    if (a.get_dischargeDate() != -1)
-        return true;
-    return false;
+// setters
+void Patient::set_firstName(string a) {
+    firstName = a;
+}
+void Patient::set_lastName(string a) {
+    lastName = a;
+}
+void Patient::set_id(long int a) {
+    id = a;
+}
+void Patient::set_doctor(long int a) {
+    doctor = a;
+}
+void Patient::set_birthday(string a) {
+    birthday = a;
+}
+void Patient::set_bloodType(string a) {
+    bloodType = a;
+}
+void Patient::set_diagn(string a) {
+    diagn = a;
+}
+void Patient::set_admissionDate(string a) {
+    admissionDate = a;
+}
+void Patient::set_dischargeDate(string a) {
+    dischargeDate = a;
+}
+
+// Getters
+string Patient::get_firstName() {
+    return firstName;
+}
+string Patient::get_lastName() {
+    return lastName;
+}
+long int Patient::get_id() {
+    return id;
+}
+long int Patient::get_doctor() {
+    return doctor;
+}
+string Patient::get_birthday() {
+    return birthday;
+}
+string Patient::get_bloodType() {
+    return bloodType;
+}
+string Patient::get_diagn() {
+    return diagn;
+}
+string Patient::get_admissionDate() {
+    return admissionDate;
+}
+string Patient::get_dischargeDate() {
+    return dischargeDate;
+}
+
+//Others
+bool Patient::IsDischarged() {
+    return dischargeDate == "-1";
+}
+
+string Patient::Patient_Status() {
+    if (diagn.find("critical") || diagn.find("Critical") || diagn.find("CRITICAL")) {
+        return "Critical";
+    }
+    if (diagn.find("moderate") || diagn.find("Moderate") || diagn.find("MODERATE")) {
+        return "Moderate";
+    }
+    return "Stable";
+}
+
+void Patient::Print_Patient_Info() {
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+    cout << "ID: " << id << endl;
+    cout << "Assigned Doctor: " << doctor << endl;
+    cout << "Date of Birth: " << birthday << endl;
+    cout << "Blood Type: " << bloodType << endl;
+    cout << "Diagnosis: " << diagn << endl;
+    cout << "Date of Admission: " << admissionDate << endl;
+    cout << "Discharge Date: " << dischargeDate << endl;
 }
--- Patient.h
+++ Patient.h
@@ -6,63 +6,30 @@ using namespace std;
 class Patient {
 public:
     // setters
-    void set_firstName(string a) {
-        firstName = a;
-    }
-    void set_lastName(string a) {
-        lastName = a;
-    }
-    void set_id(long int a) {
-        id = a;
-    }
-    void set_doctor(long int a) {
-        doctor = a;
-    }
-    void set_birthday(string a) {
-        birthday = a;
-    }
-    void set_bloodType(string a) {
-        bloodType = a;
-    }
-    void set_diagnationDate(string a) {
-        diagn = a;
-    }
-    void set_admissionDate(string a) {
-        admissionDate = a;
-    }
-    void set_dischargeDate(string a) {
-        dischargeDate = a;
-    }
+    void set_firstName(string a);
+    void set_lastName(string a);
+    void set_id(long int a);
+    void set_doctor(long int a);
+    void set_birthday(string a);
+    void set_bloodType(string a);
+    void set_diagn(string a);
+    void set_admissionDate(string a);
+    void set_dischargeDate(string a);
 
     // Getters
-    string get_firstName() {
-        return firstName;
-    }
-    string get_lastName() {
-        return lastName;
-    }
-    long int get_id() {
-        return id;
-    }
-    long int get_doctor() {
-        return doctor;
-    }
-    string get_birthday() {
-        return birthday;
-    }
-    string get_bloodType() {
-        return bloodType;
-    }
+    string get_firstName();
+    string get_lastName();
+    long int get_id();
+    long int get_doctor();
+    string get_birthday();
+    string get_bloodType();
+    string get_diagn();
+    string get_admissionDate();
+    string get_dischargeDate();
 
-    string get_diagnationDate() {
-        return diagn;
-    }
-    string get_admissionDate() {
-        return admissionDate;
-    }
-    string get_dischargeDate() {
-        return dischargeDate;
-    }
+    bool IsDischarged();
+    string Patient_Status();
+    void Print_Patient_Info();
 
 private:
     string firstName;
@@ -75,8 +42,4 @@ private:
     string admissionDate;
     string dischargeDate;
 };
-
-bool IsDischarged(Patient a);
-string Patient_Status();
-void Print_Patient_Info();
 #endif
